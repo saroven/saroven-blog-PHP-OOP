b3ea93a4dff0f2b47e708ba3e0ae4a1a2464f5d5
saroven <ashah3562@gmail.com>
2020-05-01 02:07:18 +0600

    updated 404 page

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+  include 'include/header.php';
+  include 'include/navbar.php';
+?>
+
+<section class="blog-post-area section-margin mt-4">
+      <div class="container">
+        <div class="col-md-12">
+            <h1>404 error !</h1>
+            <p>The content you are loking in the page is not available.</p>
+        </div>
+      </div>
+</section>
+
+<?php
+ include 'include/footer.php';
+?>
+
