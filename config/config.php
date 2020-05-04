ac2b0988d7a07422d54826a02cc3d8b223789c85
saroven <ashah3562@gmail.com>
2020-05-05 03:14:58 +0600

    changefiles

--- config/config.php
+++ config/config.php
@@ -3,4 +3,9 @@
 	define("DB_USER", "root");
 	define("DB_PASS", "");
 	define("DB_NAME", "blog_db");
+
+	// $base_url = $base_url = "";
+
+	define('base_url', 'http://localhost:8000/');
+
  ?>
--- helper/functions.php
+++ helper/functions.php
@@ -1,5 +1,4 @@
 <?php 
-	$base_url = $base_url = "http://localhost:8000/";
 	function goToError($url){
 		$url = $base_url."/404.php";
     	echo "<script> location.href='".$url."'; </script>";
