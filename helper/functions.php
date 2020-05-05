950476d67e391b5a22aa3abf688cbc072d42b2a2
saroven <ashah3562@gmail.com>
2020-05-05 23:24:59 +0600

    changes alert style

--- helper/functions.php
+++ helper/functions.php
@@ -1,22 +1,26 @@
 <?php 
-	$base_url = $base_url = "http://localhost:8000/";
-	function goToError($url){
-		$url = $base_url."/404.php";
-    	echo "<script> location.href='".$url."'; </script>";
-	}
-	function goToUrl($url)
-	{
-		$url = $base_url + $url;
-		echo "<script> location.href='".$url."'; </script>";
-	}
+	// $base_url = $base_url = "http://localhost:8000/";
+	// function goToError($url){
+	// 	$url = $base_url."/404.php";
+    // 	echo "<script> location.href='".$url."'; </script>";
+	// }
+	// function goToUrl($url)
+	// {
+	// 	$url = $base_url + $url;
+	// 	echo "<script> location.href='".$url."'; </script>";
+	// }
 	function success($text)
 	{
-		echo "<div class='alert alert-success' role='alert'>$text</div>";
+		echo "<div class='alert alert-success alert-dismissible'>
+	    <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+	    <h4><i class='icon fa fa-check'></i> Success</h4>$text</div>";
 
 	}
 	function failed($text)
 	{
-		echo "<div class='alert alert-danger' role='alert'>$text</div>";
+		echo "<div class='alert alert-danger alert-dismissible'>
+	    <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+	    <h4><i class='icon fa fa-check'></i> Success</h4>$text</div>";
 
 	}
  ?>
