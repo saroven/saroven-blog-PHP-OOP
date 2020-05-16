1f10403d929ac9a5cfbd9aafb1d5b0173b27027b
saroven <ashah3562@gmail.com>
2020-05-16 23:57:27 +0600

    changes user template footer credit

--- include/footer.php
+++ include/footer.php
@@ -70,7 +70,7 @@
       </div>
       <div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
         <p class="footer-text m-0">
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with   <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank"> Colorib</a> | Blog site developed by <a href="https://facebook.com/tawfiquebd" target="_blank">Md Tawfique Hossain</a></p>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with   <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank"> Colorib</a> | Blog site developed by <a href="https://facebook.com/sarovenbd" target="_blank">Mohammad Shah Alam</a></p>
       </div>
     </div>
   </footer>
