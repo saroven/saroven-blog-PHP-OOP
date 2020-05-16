6fbdc5677aab346f443fe55dc0e7e691bb547153
saroven <ashah3562@gmail.com>
2020-05-17 01:25:58 +0600

    view and update social media link from admin panel

--- admin/social.php
+++ admin/social.php
@@ -2,6 +2,37 @@
 include 'inc/header.php';
 include 'inc/sidebar.php';
 ?>
+<?php
+    $query = "SELECT * FROM socials WHERE id=1";
+    $result = $db->select($query);
+?>
+
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $fb = $_POST['fb'];
+        $twi = $_POST['twi'];
+        $ins = $_POST['ins'];
+        $pin = $_POST['pin'];
+        $fb = $db->link->real_escape_string($fb);
+        $twi = $db->link->real_escape_string($twi);
+        $ins = $db->link->real_escape_string($ins);
+        $pin = $db->link->real_escape_string($pin);
+
+        $query = "UPDATE socials
+                SET fb = '$fb',
+                twi = '$twi',
+                ins = '$ins',
+                pin = '$pin'
+                WHERE id=1";
+        $update = $db->update($query);
+        if ($update) {
+            $_SESSION['success'] = 'Updated successfully.';
+            goToUrl('social.php');
+        }else{
+            $_SESSION['error'] = 'Faild to update! Please try again.';
+        }
+    }
+?>
 
             <!-- Content Wrapper. Contains page content -->
             <div class="content-wrapper">
@@ -22,24 +53,27 @@ include 'inc/sidebar.php';
                     <div class="col-md-12">
                         <!-- general form elements -->
                         <div class="box box-primary">
+                            <?php include '../helper/message.php' ?>
                             <!-- form start -->
-                            <form role="form">
+                            <?php if ($result) {
+                                while ($social = $result->fetch_assoc()) { ?>
+                            <form role="form" method="post" action="social.php">
                                 <div class="box-body">
                                     <div class="form-group">
                                         <label for="facebook">Facebook</label>
-                                        <input type="text" name="fb" class="form-control" id="facebook" placeholder="Facebook">
+                                        <input type="text" value="<?php echo $social['fb'] ?>" name="fb" class="form-control" id="facebook" placeholder="Facebook">
                                     </div>
                                     <div class="form-group">
                                         <label for="twitter">Twitter</label>
-                                        <input type="text" name="twi" class="form-control" id="twitter" placeholder="Twitter">
+                                        <input type="text" value="<?php echo $social['twi'] ?>" name="twi" class="form-control" id="twitter" placeholder="Twitter">
                                     </div>
                                     <div class="form-group">
                                         <label for="Instagram">Instagram</label>
-                                        <input type="text" name="ins" class="form-control" id="Instagram" placeholder="Instagram">
+                                        <input type="text" value="<?php echo $social['ins'] ?>" name="ins" class="form-control" id="Instagram" placeholder="Instagram">
                                     </div>
                                     <div class="form-group">
                                         <label for="pinterest">Pinterest</label>
-                                        <input type="text" name="pin" class="form-control" id="pinterest" placeholder="pinterest">
+                                        <input type="text" value="<?php echo $social['pin'] ?>" name="pin" class="form-control" id="pinterest" placeholder="pinterest">
                                     </div>
                                 </div>
                                 <!-- /.box-body -->
@@ -48,6 +82,7 @@ include 'inc/sidebar.php';
                                     <button type="submit" class="btn btn-primary">Submit</button>
                                 </div>
                             </form>
+                            <?php      } } ?>
                         </div>
                         <!-- /.box -->
                     </div>
