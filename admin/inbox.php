f978c19a0fe9b9e89b005ae25202c58c9a03013b
saroven <ashah3562@gmail.com>
2020-07-19 00:25:03 +0600

    changes inbox page layout and added single contact message page

--- admin/inbox.php
+++ admin/inbox.php
@@ -1,93 +1,123 @@
 <?php
-    include 'inc/header.php';
-    include 'inc/sidebar.php';
- ?>
+include 'inc/header.php';
+include 'inc/sidebar.php';
+?>
 
-            <!-- Content Wrapper. Contains page content -->
-            <div class="content-wrapper">
-                <!-- Content Header (Page header) -->
-                <section class="content-header">
-                    <h1>
-                        View Messages
-                    </h1>
-                    <ol class="breadcrumb">
-                        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                        <li class="active">Dashboard</li>
-                    </ol>
-                </section>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Read Mail
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Mailbox</li>
+    </ol>
+</section>
 
-                <!-- Main content -->
-                <section class="content">
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+    <div class="col-md-3">
+      <a href="compose.html" class="btn btn-primary btn-block margin-bottom">Compose</a>
 
-                    <div class="col-md-12">
-                        <!-- general form elements -->
-                        <div class="box box-primary">
-                            <div class="box">
-                                <div class="box-header">
-                                    <h3 class="box-title">Message Lists</h3>
-                                </div>
-                                <!-- /.box-header -->
-                                <div class="box-body">
-                                    <table id="example1" class="table table-bordered table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th>Serial No.</th>
-                                                <th>Sender Name</th>
-                                                <th>Subject</th>
-                                                <th>Sender Email</th>
-                                                <th>Message</th>
-                                                <th>Date</th>
-                                                <th>Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                                $query = "SELECT * FROM contacts ORDER BY id DESC";
-                                                $contact = $db->select($query);
-                                                if ($contact) {
-                                                    $i = 0;
-                                                    while ($result = $contact->fetch_assoc()) {
-                                                        $i++;
-                                             ?>
-                                            <tr>
-                                                <td><?php echo $i; ?></td>
-                                                <td><?php echo $result['name']; ?></td>
-                                                <td><?php echo $result['subject']; ?></td>
-                                                <td><?php echo $result['email']; ?></td>
-                                                <td><?php echo $fm->textShorten($result['text']); ?></td>
-                                                <td><?php echo $fm->formatDate($result['date']);?></td>
-                                                <td><a href="viewmsg.php?msgid=<?php echo $result['id'] ?>">View</a> | <a href="replymsg.php?msgid=<?php echo $result['id'] ?>">Reply</a> | <a href="?seen=<?php echo $result['id'] ?>">Seen</a> | <a href="deletemsg.php?msgid=<?php echo $result['id'] ?>">Delete</a></td>
-                                            </tr>
-                                        <?php }} ?>
-                                        </tbody>
-                                        <tfoot>
-                                            <tr>
-                                                <th>Serial No.</th>
-                                                <th>Sender Name</th>
-                                                <th>Subject</th>
-                                                <th>Sender Email</th>
-                                                <th>Message</th>
-                                                <th>Date</th>
-                                                <th>Action</th>
-                                            </tr>
-                                        </tfoot>
-                                    </table>
-                                </div>
-                                <!-- /.box-body -->
-                            </div>
-                        </div>
-                        <!-- /.box -->
-                    </div>
+      <div class="box box-solid">
+        <div class="box-header with-border">
+          <h3 class="box-title">Folders</h3>
 
+          <div class="box-tools">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+            </button>
+        </div>
+    </div>
+    <div class="box-body no-padding">
+      <ul class="nav nav-pills nav-stacked">
+        <li><a href="#"><i class="fa fa-inbox"></i> Inbox
+          <span class="label label-primary pull-right">12</span></a></li>
+          <li><a href="#"><i class="fa fa-envelope-o"></i> Sent</a></li>
+          <li><a href="#"><i class="fa fa-file-text-o"></i> Drafts</a></li>
+          <li><a href="#"><i class="fa fa-filter"></i> Junk <span class="label label-warning pull-right">65</span></a>
+          </li>
+          <li><a href="#"><i class="fa fa-trash-o"></i> Trash</a></li>
+      </ul>
+  </div>
+  <!-- /.box-body -->
+</div>
 
-                </section>
-                <!-- /.content -->
+</div>
+<!-- /.col -->
+<div class="col-md-9">
+    <!-- general form elements -->
+    <div class="box box-primary">
+        <div class="box">
+            <div class="box-header">
+                <?php include '../helper/message.php';?>
+                <h3 class="box-title">Message Lists</h3>
             </div>
-            <!-- /.content-wrapper -->
-            
+            <!-- /.box-header -->
+            <div class="box-body">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Serial No.</th>
+                            <th>Sender Name</th>
+                            <th>Subject</th>
+                            <th>Sender Email</th>
+                            <th>Message</th>
+                            <th>Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $query = "SELECT * FROM contacts ORDER BY id DESC";
+                        $contact = $db->select($query);
+                        if ($contact) {
+                            $i = 0;
+                            while ($result = $contact->fetch_assoc()) {
+                                $i++;
+                                ?>
+                                <tr>
+                                    <td><?php echo $i; ?></td>
+                                    <td><?php echo $result['name']; ?></td>
+                                    <td><?php echo $result['subject']; ?></td>
+                                    <td><?php echo $result['email']; ?></td>
+                                    <td><?php echo $fm->textShorten($result['text']); ?></td>
+                                    <td><?php echo $fm->formatDate($result['date']);?></td>
+                                    <td><a href="viewmsg.php?id=<?php echo $result['id'] ?>">View</a> | <a href="replymsg.php?id=<?php echo $result['id'] ?>">Reply</a> | <a href="?seen=<?php echo $result['id'] ?>">Seen</a> | <a href="deletemsg.php?msgid=<?php echo $result['id'] ?>">Delete</a></td>
+                                </tr>
+                            <?php }} ?>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Serial No.</th>
+                                <th>Sender Name</th>
+                                <th>Subject</th>
+                                <th>Sender Email</th>
+                                <th>Message</th>
+                                <th>Date</th>
+                                <th>Action</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+                <!-- /.box-body -->
+            </div>
+        </div>
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
+</div>
+<!-- /.row -->
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
 <?php
-    include 'inc/footer.php';
- ?>
+include 'inc/footer.php';
+?>
 <!-- DataTables -->
 <script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
@@ -95,13 +125,13 @@
     $(function () {
 
         //data table
-      $('#example1').DataTable()
+        $('#example1').DataTable()
         
       // nav highlight function
 
       // nav_highlight('category','add-category');
 
-    });
-  </script>
-    })
+  });
+</script>
+})
 </script>
---
+++ admin/viewmsg.php
@@ -0,0 +1,132 @@
+<?php
+include 'inc/header.php';
+include 'inc/sidebar.php';
+?>
+
+<?php
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $id = $_GET['id'];
+    $query = "SELECT * FROM contacts WHERE id='$id'";
+    $result = $db->select($query);
+    $contact = $result->fetch_assoc();
+
+}else{
+    $_SESSION['error'] = 'Something went wrong! please try again.';
+    goToUrl('inbox.php');
+}
+
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Read Mail
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Mailbox</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+    <div class="col-md-3">
+      <a href="compose.html" class="btn btn-primary btn-block margin-bottom">Compose</a>
+
+      <div class="box box-solid">
+        <div class="box-header with-border">
+          <h3 class="box-title">Folders</h3>
+
+          <div class="box-tools">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+            </button>
+        </div>
+    </div>
+    <div class="box-body no-padding">
+      <ul class="nav nav-pills nav-stacked">
+        <li><a href="mailbox.html"><i class="fa fa-inbox"></i> Inbox
+          <span class="label label-primary pull-right">12</span></a></li>
+          <li><a href="#"><i class="fa fa-envelope-o"></i> Sent</a></li>
+          <li><a href="#"><i class="fa fa-file-text-o"></i> Drafts</a></li>
+          <li><a href="#"><i class="fa fa-filter"></i> Junk <span class="label label-warning pull-right">65</span></a>
+          </li>
+          <li><a href="#"><i class="fa fa-trash-o"></i> Trash</a></li>
+      </ul>
+  </div>
+  <!-- /.box-body -->
+</div>
+<!-- /. box -->
+
+</div>
+<!-- /.col -->
+<div class="col-md-9">
+  <div class="box box-primary">
+    <div class="box-header with-border">
+      <h3 class="box-title">Read Mail</h3>
+
+      <div class="box-tools pull-right">
+        <a href="#" class="btn btn-box-tool" data-toggle="tooltip" title="Previous"><i class="fa fa-chevron-left"></i></a>
+        <a href="#" class="btn btn-box-tool" data-toggle="tooltip" title="Next"><i class="fa fa-chevron-right"></i></a>
+    </div>
+</div>
+<!-- /.box-header -->
+<div class="box-body no-padding">
+  <div class="mailbox-read-info">
+    <h3><?php echo $contact['subject']; ?></h3>
+    <h5>From: <?php echo $contact['email']; ?>
+      <span class="mailbox-read-time pull-right"><?php echo $fm->formatDate($contact['date']); ?></span></h5>
+  </div>
+  <!-- /.mailbox-read-info -->
+
+      <!-- /.mailbox-controls -->
+    <div class="mailbox-read-message">
+        sdfsadfsdfsdf
+    </div>
+        <!-- /.mailbox-read-message -->
+    </div>
+    <!-- /.box-body -->
+
+<!-- /.box-footer -->
+<div class="box-footer">
+  <div class="pull-right">
+    <button type="button" class="btn btn-default"><i class="fa fa-reply"></i> Reply</button>
+    <button type="button" class="btn btn-default"><i class="fa fa-share"></i> Forward</button>
+</div>
+<button type="button" class="btn btn-default"><i class="fa fa-trash-o"></i> Delete</button>
+<button type="button" class="btn btn-default"><i class="fa fa-print"></i> Print</button>
+</div>
+<!-- /.box-footer -->
+</div>
+<!-- /. box -->
+</div>
+<!-- /.col -->
+</div>
+<!-- /.row -->
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+include 'inc/footer.php';
+?>
+<!-- DataTables -->
+<script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script>
+    $(function () {
+
+        //data table
+        // $('#example1').DataTable()
+        
+      // nav highlight function
+
+      // nav_highlight('category','add-category');
+
+  });
+</script>
+})
+</script>
