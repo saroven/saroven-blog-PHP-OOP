53c9735757adc4fd41684c9b05464a7b6020fd3e
saroven <ashah3562@gmail.com>
2020-09-12 01:14:01 +0600

    show slider data from database dynamicly

--- include/slider.php
+++ include/slider.php
@@ -1,30 +1,26 @@
  <!--================Hero Banner start =================-->
+<?php  
+   $sql = "SELECT * FROM posts LIMIT 4";
+    $posts = $db->select($sql);
+  ?>
  <section>
       <div class="container">
         <div class="owl-carousel owl-theme blog-slider">
+        <?php if ($posts != false) {
+          while ($post = $posts->fetch_assoc()) { ?>
 
           <div class="card blog__slide text-center">
-            <a href="post.html">
+            <a href="post.php?id=<?php echo $post['id'] ?>">
               <div class="blog__slide__img">
-                <img class="card-img rounded-0 img-responsive" src="img/blog/blog-slider/blog-slide1.png" alt="">
+                <img class="card-img rounded-0 img-responsive" src="admin/<?php echo $post['image'] ?>" alt="">
               </div>
               <div class="blog__slide__content">
-                <h2>This is post heading or title</h2>
+                <h2><?php echo $post['title']; ?></h2>
               </div>
             </a>
           </div>
 
-          <div class="card blog__slide text-center">
-            <a href="post.html">
-              <div class="blog__slide__img">
-                <img class="card-img rounded-0 img-responsive" src="img/blog/blog-slider/blog-slide1.png" alt="">
-              </div>
-              <div class="blog__slide__content">
-                <h2>This is post heading or title</h2>
-              </div>
-            </a>
-          </div>
-
+        <?php } } ?>
         </div>
       </div>
     </section>
